08fdabdc70b726d2fc2ebf7fe9a18c536bc50e87
iantreanor <trei1_15@uni.worc.ac.uk>
2016-10-20 10:59:02 +0100

    d
    d

--- www/js/index.js
+++ www/js/index.js
@@ -1,33 +1,14 @@
-var value = window.localStorage.getItem(Book);
-var value = window.localStorage.getItem(Pen);
-var value = window.localStorage.getItem(Phone);
-var value = window.localStorage.getItem(Keys);
-var value = window.localStorage.getItem(Laptop);
+var celebration = {
+    "chocbar":{
+	"Energy kj":"2068",
+    "Fat ":"24.4",
+	"Carbohydrate": "62.3",
+	"Protein ": "5.5",
+	"Salt": "0.40"
+    }	
+}
 
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#book").text("book");
-		$("#pen").text("Pen");
-		$("#phone").text("Phone");
-		$("#Keys").text("Keys");
-		$("#Laptop").text("Laptop");
-	}
+obj = JSON.parse(celebration);
 
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
-        
-		window.localStorage.getItem( Book, Severn );
-		window.localStorage.getItem( Pen, Black );
-		window.localStorage.getItem( Phone, iPhone );
-		window.localStorage.getItem( keys, Car );
-		window.localStorage.getItem( Laptop, Windows );
-		
-		updateDisplay();
-    }
+document.getElementById("chocbar").innerHTML =
+obj.chocbar.Salt;
